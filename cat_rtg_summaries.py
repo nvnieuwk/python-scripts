deded48284d453f8a0478c0296a70fd587ebc1d1
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2023-11-06 15:52:16 +0100

    add output argument to cat_rtg_summaries

--- cat_rtg_summaries.py
+++ cat_rtg_summaries.py
@@ -14,20 +14,21 @@ if __name__ == "__main__":
     # Setting up argparser
     parser = argparse.ArgumentParser(description="Concatenate all RTG VCFeval summary files")
     parser.add_argument('input', metavar='FOLDER', type=str, help="The folder containing all input files")
+    parser.add_argument('output', metavar='FILE', type=str, help="The output CSV file")
 
     args = parser.parse_args()
 
     input = args.input
+    output_file = args.output
 
     header = ["samplename", "threshold", "true_positives_baseline", "true_positives_called", "false_positives", "false_negatives", "precision", "sensitivity", "f-measure"]
-    output = open("full_summary.csv", "w")
+    output = open(output_file, "w")
     writer = csv.writer(output)
     writer.writerow(header)
 
     for subdir, dirs, files in os.walk(input):
         summaries = get_summaries(files)
         if len(summaries) == 0: continue
-        print(summaries)
 
         for summary in summaries:
             sample_name = re.search('^([^\.]+)\..*$', summary).group(1)
